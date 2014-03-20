10cbcaccb2074c0780f2098642ad5f2a11f12e07
stefano <stefano@10.165.68.20>
2014-03-20 16:50:19 +0100

    added portable jndi name

--- parent-web-ejb/ejb-client/src/main/java/net/tinvention/training/ejb/MainClient.java
+++ parent-web-ejb/ejb-client/src/main/java/net/tinvention/training/ejb/MainClient.java
@@ -21,14 +21,21 @@ import net.tinvention.training.ejb.stateless.Echo;
 
 
 public class MainClient  {
+	
+	 private final static String JNDI_ECHOBEAN_REMOTE_URI = "java:global/ear-web-ejb/ejb/EchoBean!" + Echo.class.getName();
 
 	 public static void main(String args[]) {
 
 	        try {
 
 	            InitialContext ic = new InitialContext();
-	            Echo echo = (Echo) ic.lookup(Echo.class.getName()); //Same host, Glassfish default
+	            // Echo echo = (Echo) ic.lookup(Echo.class.getName()); //Same host, Glassfish default
+	            Echo echo = (Echo) ic.lookup(JNDI_ECHOBEAN_REMOTE_URI); 
 	            System.out.println("Echo bean says : " + echo.echoMsg("Test echo ...."));
+	            
+	            // For connecting to a different remote host you need to set
+                // -Dorg.omg.CORBA.ORBInitialHost=xxx -Dorg.omg.CORBA.ORBInitialPort=xxxx
+	            // ex. -Dorg.omg.CORBA.ORBInitialHost=yourdomain.your -Dorg.omg.CORBA.ORBInitialPort=3700
 
 	        } catch(Exception e) {
 	            e.printStackTrace();
